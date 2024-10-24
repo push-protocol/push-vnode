91be5d6b100feb32408f2414c1b04d9b97de2c6a
Igx22 <swein2@gmail.com>
2024-10-24 13:46:21 +0700

    add: deploy script for snodes (1)

--- docker/deployctl.py
+++ docker/deployctl.py
@@ -2,6 +2,9 @@ import os
 import subprocess
 import pty
 from time import sleep
+import sys
+
+# FRAMEWORK ------------------------------------------------------------------------------------------------------------
 
 def cmdi(command, cwd=None):
     argv = ["sh", "-c", command]
@@ -22,7 +25,6 @@ def cmdi(command, cwd=None):
     finally:
         os.chdir(original_cwd)
 
-
 def cmd(command, cwd=None):
     process = subprocess.Popen(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd, universal_newlines=True
@@ -33,8 +35,11 @@ def cmd(command, cwd=None):
     if process.returncode != 0:
         raise subprocess.CalledProcessError(process.returncode, command)
 
-def main():
+# COMMANDS WITH PARAMS -------------------------------------------------------------------------------------------------
+
+def deployValidator():
     # cmdi('docker build . -t vnode-main', '/Users/w/chain/push-vnode')
+    print("deploying vnode code on DEV environment")
     dir_vnode = '/home/chain/source/push-vnode'
     dir_yml = '/home/chain'
 
@@ -55,9 +60,66 @@ def main():
     print("Displaying the last 200 lines of Docker Compose logs...")
     cmdi('docker compose -f v.yml logs | tail -n 200', dir_yml)
 
+def deployStorage():
+    print("deploying snode code on DEV environment")
+    # cmdi('docker build . -t vnode-main', '/Users/w/chain/push-vnode')
+    dir_snode = '/home/chain/source/push-snode'
+    dir_yml = '/home/chain'
+
+    # cmdi('ls -la')
+    # exit(0) # todo remove
+
+    cmdi('git config credential.helper store', dir_snode)
+    cmdi('git pull', dir_snode)
+    sleep(10)
+
+    cmdi('docker build . -t snode-main', dir_snode)
+    sleep(10)
+    cmdi('docker compose -f s.yml down', dir_yml)
+    sleep(10)
+    cmdi('docker compose -f s.yml up -d', dir_yml)
+    sleep(10)
+
+    print("Displaying the last 200 lines of Docker Compose logs...")
+    cmdi('docker compose -f s.yml logs | tail -n 200', dir_yml)
+
+def test(msg1, msg2):
+    print(msg1, msg2)
+
+
+# FRAMEWORK ------------------------------------------------------------------------------------------------------------
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: script.py cmd [cmdParams...]")
+        sys.exit(1)
+    cmd = sys.argv[1]
+    cmdParams = sys.argv[2:]
+
+    # Map cmd to function
+    # ADD COMMANDS HERE !!!!!
+    commands = {
+        'deployValidator': deployValidator,
+        'deployStorage': deployStorage,
+        'test': test
+    }
+
+    if cmd in commands:
+        func = commands[cmd]
+        try:
+            func(*cmdParams)
+        except TypeError as e:
+            print(f"Error: {e}")
+            print(f"The command '{cmd}' requires {func.__code__.co_argcount} parameters.")
+    else:
+        print(f"Unknown command: {cmd}")
+        print("Available commands: " + ", ".join(commands.keys()))
+        sys.exit(1)
+
 if __name__ == '__main__':
     try:
         main()
     except subprocess.CalledProcessError as e:
         print(f"\nAn error occurred while executing: {e.cmd}")
         print(f"Exit code: {e.returncode}")
+
